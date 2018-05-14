cb57294fea9726868eb92820f1f06bcaf40b0287
Exxenoz <exxenoz@gmx.at>
2018-05-14 03:54:50 +0200

    #25733 [Kunden anzeigen] - Fixed findAll() implementation of CustomerEndpoint class

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/endpoint/CustomerEndpoint.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/endpoint/CustomerEndpoint.java
@@ -1,25 +1,29 @@
 package at.ac.tuwien.inso.sepm.ticketline.server.endpoint;
 
 import at.ac.tuwien.inso.sepm.ticketline.rest.customer.CustomerDTO;
+import at.ac.tuwien.inso.sepm.ticketline.rest.page.PageRequestDTO;
+import at.ac.tuwien.inso.sepm.ticketline.rest.page.PageResponseDTO;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.Customer;
 import at.ac.tuwien.inso.sepm.ticketline.server.entity.mapper.customer.CustomerMapper;
 import at.ac.tuwien.inso.sepm.ticketline.server.service.CustomerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/customer")
 @Api(value = "customer")
 public class CustomerEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final CustomerService customerService;
     private final CustomerMapper customerMapper;
@@ -29,11 +33,11 @@ public class CustomerEndpoint {
         this.customerMapper = customerMapper;
     }
 
-    @GetMapping("pageable")
+    @PostMapping()
     @ApiOperation("Get page of customer entries")
-    public Page<CustomerDTO> findAll(Pageable pageable) {
-        Page<Customer> customerPage = customerService.findAll(pageable);
+    public PageResponseDTO<CustomerDTO> findAll(@RequestBody final PageRequestDTO pageRequestDTO) {
+        Page<Customer> customerPage = customerService.findAll(pageRequestDTO.getPageable());
         List<CustomerDTO> customerDTOList = customerMapper.customerToCustomerDTO(customerPage.getContent());
-        return new PageImpl<>(customerDTOList, pageable, customerPage.getTotalElements());
+        return new PageResponseDTO<>(customerDTOList, customerPage.getTotalPages());
     }
 }
