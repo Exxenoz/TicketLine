dc1d11de52ee104c0ae78bae9fddfbee40f14c16
Martin Steller <martin.steller@gmail.com>
2018-05-14 21:20:39 +0200

    #25782 [Suchen/Filtern nach Veranstaltungen/Orten/Künstlern/Zeit] Fixed unit tests in-memory database setup, @SpringBootTest is enough.

--- server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/unittest/events/PerformanceServiceTests.java
+++ server/src/test/java/at/ac/tuwien/inso/sepm/ticketline/server/unittest/events/PerformanceServiceTests.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,6 +20,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("unit-test")
 public class PerformanceServiceTests {
 
