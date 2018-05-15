acd3abce07c44573b7814b3d1cdb93bedf71a95d
Edith Sarau <christiane.sarau@gmail.com>
2018-05-15 16:58:09 +0200

    bad merge fix

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/RefillDB.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/RefillDB.java
@@ -15,7 +15,6 @@ public class RefillDB {
         ConfigurableApplicationContext context = SpringApplication.run(RefillDB.class, args);
 
         EventRepository eventRepository = context.getBean(EventRepository.class);
-        ArtistRepository artistRepository = context.getBean(ArtistRepository.class);
         NewsRepository newsRepository = context.getBean(NewsRepository.class);
         PerformanceRepository performanceRepository = context.getBean(PerformanceRepository.class);
         ReservationRepository reservationRepository = context.getBean(ReservationRepository.class);
@@ -29,8 +28,6 @@ public class RefillDB {
             new SectorCategoryDataGenerator(sectorCategoryRepository),
             new SectorDataGenerator(sectorRepository, sectorCategoryRepository),
             new SeatDataGenerator(seatRepository, sectorRepository),
-            new ArtistDataGenerator(artistRepository),
-            new EventDataGenerator(eventRepository, artistRepository),
             new PerformanceDataGenerator(performanceRepository, eventRepository),
             new NewsDataGenerator(newsRepository),
             new ReservationDataGenerator(reservationRepository, seatRepository, performanceRepository)
@@ -43,12 +40,10 @@ public class RefillDB {
         sectorCategoryRepository.deleteAll();
         performanceRepository.deleteAll();
         eventRepository.deleteAll();
-        artistRepository.deleteAll();
         newsRepository.deleteAll();
         usersRepository.deleteAll();
 
         //CompositeDataGenerator
         compositeDataGenerator.generate();
-        System.exit(0);
     }
 }
--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/repository/ArtistRepository.java
+++
@@ -1,9 +0,0 @@
-package at.ac.tuwien.inso.sepm.ticketline.server.repository;
-
-import at.ac.tuwien.inso.sepm.ticketline.server.entity.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-}
