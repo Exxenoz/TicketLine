3af0f232859938f29cc806b1f5389985ba55391a
Max Ernst <mernst32@yahoo.com>
2018-05-19 11:58:25 +0200

    #24940 [Authentifizierung] - made UserValidator more modular;

--- rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/validator/UserValidator.java
+++ rest/src/main/java/at/ac/tuwien/inso/sepm/ticketline/rest/validator/UserValidator.java
@@ -6,30 +6,84 @@ import at.ac.tuwien.inso.sepm.ticketline.rest.util.RestBundleManager;
 
 public abstract class UserValidator {
 
-    public static void validateUser(UserDTO userDTO) throws UserValidatorException {
+    //validates existing user
+    public static void validateExistingUser(UserDTO userDTO) throws UserValidatorException {
+        validateDTO(userDTO);
+        validateUsername(userDTO);
+        validateEncryptedPassword(userDTO);
+        validateStrikes(userDTO);
+    }
+
+    //validates newly created user
+    public static void validateNewUser(UserDTO userDTO) throws UserValidatorException {
+        validateDTO(userDTO);
+        validateID(userDTO);
+        validateExistingUser(userDTO);
+    }
+
+    public static void validateDTO(UserDTO userDTO) throws UserValidatorException {
         if (userDTO == null) {
             throw new UserValidatorException(
                 RestBundleManager.getExceptionBundle().getString("exception.validator.user.is_null")
             );
+        }
+    }
+
+    public static void validateID(UserDTO userDTO) throws UserValidatorException {
+        validateDTO(userDTO);
+        if (userDTO.getId() == null) {
+            throw new UserValidatorException(
+                RestBundleManager.getExceptionBundle().getString("exception.validator.user.id.is_null")
+            );
         } else {
             if (userDTO.getId() <= 0) {
                 throw new UserValidatorException(
-                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.id_is_negative")
+                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.id.is_negative")
                 );
             }
+        }
+    }
+
+    public static void validateUsername(UserDTO userDTO) throws UserValidatorException {
+        validateDTO(userDTO);
+        if (userDTO.getUsername() == null) {
+            throw new UserValidatorException(
+                RestBundleManager.getExceptionBundle().getString("exception.validator.user.username.is_null")
+            );
+        } else {
             if (userDTO.getUsername().length() < 3) {
                 throw new UserValidatorException(
-                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.username_too_short")
+                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.username.too_short")
                 );
             }
+        }
+    }
+
+    public static void validateEncryptedPassword(UserDTO userDTO) throws UserValidatorException {
+        validateDTO(userDTO);
+        if (userDTO.getPassword() == null) {
+            throw new UserValidatorException(
+                RestBundleManager.getExceptionBundle().getString("exception.validator.user.password.is_null")
+            );
+        } else {
             if (userDTO.getPassword().length() != 60) {
                 throw new UserValidatorException(
-                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.password_invalid_length")
+                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.password.invalid_length")
                 );
             }
+        }
+    }
+
+    public static void validateStrikes(UserDTO userDTO) throws UserValidatorException {
+        validateDTO(userDTO);
+        if (userDTO.getStrikes() == null) {
+            throw new UserValidatorException(
+                RestBundleManager.getExceptionBundle().getString("exception.validator.user.strikes.is_null")
+            );
+        } else {
             if ((userDTO.getStrikes() < 0) && (userDTO.getStrikes() > 5)) {
                 throw new UserValidatorException(
-                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.invalid_strikes")
+                    RestBundleManager.getExceptionBundle().getString("exception.validator.user.strikes.invalid")
                 );
             }
         }
