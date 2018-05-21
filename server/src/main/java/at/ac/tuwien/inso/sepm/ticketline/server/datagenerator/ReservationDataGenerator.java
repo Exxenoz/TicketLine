c9ae9c35f816eee7d326f6276688421e223626a6
Martin Steller <martin.steller@gmail.com>
2018-05-21 19:00:27 +0200

    #25810 [Ticketreservierungen] Fixed @PostConstruct import.

--- server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/datagenerator/ReservationDataGenerator.java
+++ server/src/main/java/at/ac/tuwien/inso/sepm/ticketline/server/datagenerator/ReservationDataGenerator.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
 
 import java.lang.invoke.MethodHandles;
 import java.time.LocalDate;
