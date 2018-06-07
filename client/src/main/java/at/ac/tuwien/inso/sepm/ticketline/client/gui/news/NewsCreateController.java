8c7e6c55fe42a9c5f6f3519b0dd99fdf12a3fd02
Flo <me@flo-lan.com>
2018-06-07 23:10:22 +0200

    #28171 [News erstellen] - implemented creating news formular template

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/news/NewsCreateController.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/gui/news/NewsCreateController.java
@@ -1,4 +1,30 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.gui.news;
 
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.MainController;
+import at.ac.tuwien.inso.sepm.ticketline.client.gui.TabHeaderController;
+import at.ac.tuwien.inso.sepm.ticketline.client.service.NewsService;
+import javafx.fxml.FXML;
+import org.springframework.stereotype.Component;
+
+import static org.controlsfx.glyphfont.FontAwesome.Glyph.NEWSPAPER_ALT;
+
+@Component
 public class NewsCreateController {
+
+    @FXML
+    private TabHeaderController tabHeaderController;
+
+    private final MainController mainController;
+    private final NewsService newsService;
+
+    public NewsCreateController(MainController mainController, NewsService newsService) {
+        this.mainController = mainController;
+        this.newsService = newsService;
+    }
+
+    @FXML
+    private void initialize() {
+        tabHeaderController.setIcon(NEWSPAPER_ALT);
+        tabHeaderController.setTitle("Create News");
+    }
 }
