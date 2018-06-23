8b756c04620428d4ff6822a1c70ed3b87df78c4c
Exxenoz <exxenoz@gmx.at>
2018-06-23 03:48:44 +0200

    #27784 [Mehrsprachigkeit] - Implemented dynamic i18n for reservations

--- client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/exception/CustomerValidationException.java
+++ client/src/main/java/at/ac/tuwien/inso/sepm/ticketline/client/exception/CustomerValidationException.java
@@ -1,10 +1,19 @@
 package at.ac.tuwien.inso.sepm.ticketline.client.exception;
 
 public class CustomerValidationException extends Exception {
+
+    private String exceptionBundleKey;
+
     public CustomerValidationException() {
     }
 
-    public CustomerValidationException(String message) {
+    public CustomerValidationException(String message, String exceptionBundleKey) {
         super(message);
+
+        this.exceptionBundleKey = exceptionBundleKey;
+    }
+
+    public String getExceptionBundleKey() {
+        return exceptionBundleKey;
     }
 }
