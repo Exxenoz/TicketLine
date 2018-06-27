c4e1eb9599ad6ca0935b65e805bc1e73307025ea
susi <susi.rinortner@gmail.com>
2018-06-27 22:04:50 +0200

    #29211 [Validatortests] - validator test for location address data

---
+++ client/src/test/java/at/ac/tuwien/inso/sepm/ticketline/client/validator/LocationAddressValidatorTest.java
@@ -0,0 +1,47 @@
+package at.ac.tuwien.inso.sepm.ticketline.client.validator;
+
+
+import at.ac.tuwien.inso.sepm.ticketline.client.exception.AddressValidationException;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.control.TextField;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("unit-test")
+public class LocationAddressValidatorTest {
+
+
+    private final JFXPanel fxPanel = new JFXPanel();
+
+    @Test
+    public void validateLocationNameTest() throws AddressValidationException {
+        TextField textField = new TextField();
+        textField.setText("Name");
+        LocationAddressValidator.validateLocationName(textField);
+    }
+
+    @Test(expected = AddressValidationException.class)
+    public void validateLocationNameTooShortTest() throws AddressValidationException {
+        TextField textField = new TextField();
+        textField.setText("");
+        LocationAddressValidator.validateLocationName(textField);
+    }
+
+    @Test(expected = AddressValidationException.class)
+    public void validateLocationNameTooLongTest() throws AddressValidationException {
+        TextField textField = new TextField();
+        textField.setText("veBE2yN1cXvyMQahtUfppjH5x3MioudGCNHA5io6ZGFwkEJKQxN");
+        LocationAddressValidator.validateLocationName(textField);
+    }
+
+    @Test(expected = AddressValidationException.class)
+    public void validateLocationNameInvalidCharactersTest() throws AddressValidationException {
+        TextField textField = new TextField();
+        textField.setText("ad*");
+        LocationAddressValidator.validateLocationName(textField);
+    }
+
+}
